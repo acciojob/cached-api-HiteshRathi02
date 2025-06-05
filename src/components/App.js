f5844e4494780681d162c5628058cbe2e0f50376
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-06-05 16:15:30 +0000

    functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
+import axios from "axios";
+
 
 const App = () => {
+  const [loading,setloading]=useState(true);
+  const [input, setInput] = useState("https://jsonplaceholder.typicode.com/posts")
+  const [data,setdata]=useState([]);
+
+  const data1=useMemo(()=>{
+    return data},
+        [input]);
+ 
+  useEffect(()=>{
+    axios.get(input).then((res)=>{
+      setdata(res.data);
+      setloading(false);
+    });
+},[data]);
+
+  
+ 
   return (
     <div>
-        {/* Do not remove the main div */}
+      {loading && <div>Loading...</div> }
+      <ul>
+      {
+  data.map((item,i)=> 
+  <li key={item.id}>
+  <h4>{item.title}</h4>
+  <p>{item.body}</p>
+</li>
+  )
+}
+      </ul>
     </div>
   )
 }
